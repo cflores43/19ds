3bd7af1948bf0ee453a7d429d78197e365e41772
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-13 16:38:18 -0500

    Update and rename 2.-base7.js to 02.-base7.js

--- 02.-base7.js
+++ 02.-base7.js
@@ -2,5 +2,5 @@ let res
 const log7 = x =>  Math.log10(x)/Math.log10(7) 
 for (let i = 0; i <=10;i++){
     res = log7(i)
-    console.log(`El resultado ${i} es `+ res)
+    console.log(`Resultado ${i} es `+ res)
 }
