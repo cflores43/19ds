d4a8a7a6c9d342dfdc4373cf6d2d95d8dfae3717
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-13 16:26:53 -0500

    Update 1.-base_b.js

--- 1.-base_b.js
+++ 1.-base_b.js
@@ -1,11 +1,7 @@
 let res
-let log = prompt('por favor, introduce la nueva base')
-
-const log7 = (x,y)=>  Math.log10(x)/Math.log10(y) 
-
+let log = prompt('Introduce la nueva base')
+const log7 = (x,y) => Math.log10(x)/Math.log10(y) 
 for (let i = 0; i <=10;i++){
-
     res = log7(i,log)
-    console.log(`El resultado ${i} es `+ res)
-
+    console.log(`El resultado de ${i} es `+ res)
 }
