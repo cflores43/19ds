a0a3452c7ed62b499093dd575190b0448c91cf96
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-13 17:35:04 -0500

    Update and rename 12.-howmany.js to 12.-howMany.js

--- 12.-howMany.js
+++ 12.-howMany.js
@@ -1,18 +1,16 @@
 var array = [3,2,1];
 
-function howmany(x){
+function howMany(x){
     var x = x.sort(function(a, b){return a-b});
-    var iguales = 0;
+    var igl = 0;
     for(c1 = 0; c1 < x.length; c1++){
         if(x[c1]==x[c1+1]){
             for(c2 = c1+1; c2 < x.length; c2++){
-                if(x[c1]==x[c2]) iguales++;
+                if(x[c1]==x[c2]) igl++;
             }
         }
-        c1= c1+iguales;
-
+        c1= c1+igl;
     }
-    return iguales;
+    return igl;
 }
-
-howmany(array);
+howMany(array);
