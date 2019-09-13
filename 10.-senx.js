28626ed15983e0c8dec895aa52eeab03d0e1eb7e
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-13 18:12:45 -0500

    Update 10.-senx.js

--- 10.-senx.js
+++ 10.-senx.js
@@ -1,8 +1,20 @@
-function senx(x){
-
-    
-    return(Math.sin(x))
-
+function factorial(fa){
+    let res = 1;
+    for(let i = 1 ; i <= fa ; i++){
+        res = res * i;
+    }
+    return(res);
 }
 
-senx(6)
+
+//Sen x = x - x^3/3! + x^5/5!
+function senx(x){
+    let res = x;
+    for(let i = 3 ; i <= 7 ; i=i+4){
+        res = res - Math.pow(x,i)/factorial(i)
+    }
+    for(let i = 5 ; i <= 9 ; i=i+4){
+        res = res + Math.pow(x,i)/factorial(i)
+    }
+    return(res)
+}
