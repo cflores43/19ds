d4a0ebce67fe6963c861d1d4a5c83b590f9d20a8
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-13 16:37:20 -0500

    Update 3.-teamBase.js

--- 3.-teamBase.js
+++ 3.-teamBase.js
@@ -1,8 +1,9 @@
 //Team
 
-console.log ("2 ala N");
+console.log ("2 a la N");
 let y = prompt();
 
 for (let i = 1; i<=y; i++){
      console.log("2 ^ " + i + " = " + (Math.pow(2, i)));
 }
+console.log("done...")
