7c9ba891fc96e352f5898862e40c318c7137bb27
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-14 13:03:08 -0500

    Update 08a.-prove_that_e.js

--- 08a.-prove_that_e.js
+++ 08a.-prove_that_e.js
@@ -1,16 +1,14 @@
 function fac (n) {
-	let total = 1; 
+	let tot = 1; 
 	for (i=1; i<=n; i++) {
-		total = total * i; 
+		tot = tot * i; 
 	}
-	return total; 
+	return tot; 
 }
 let form = 0
 let x=1
 
 for(let i = 0; i <= 9;i++){
     form += Math.pow(x,i)/fac(i)
-
 }
-
-console.log(`El resultado es ${form}`)
+console.log("El resultado es " + form)
