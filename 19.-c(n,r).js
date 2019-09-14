b5382810d50b795e3ef057b7cb37bc9c6ecdfa15
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-14 14:42:01 -0500

    Update 19.-c(n,r).js

--- 19.-c(n,r).js
+++ 19.-c(n,r).js
@@ -1,15 +1,20 @@
-function conv (n,r) {
+let a = prompt("Introduce un numero para x")
+let x = parseInt(a)
+let b = prompt("Introduce un numero para y")
+let y = parseInt(b)
+
+function conv (x,y) {
 	var tot = 1; 
-	for (i=1; i<=n; i++) {
+	for (i=1; i<=x; i++) {
         tot = tot * i;         
     }
-    let sum = n - r
+    let sum = x - y
     let tot2 = 1;
     for (i=1; i<=sum; i++) {
         tot2 = tot2 * i; 
     }
     let tot3 = 1;
-    for (i=1; i<=r; i++) {
+    for (i=1; i<=y; i++) {
         tot3 = tot3 * i; 
     }
     let form1 = tot/tot2
@@ -18,4 +23,4 @@ function conv (n,r) {
 
 }
 
-console.log(`El resultado 1>${conv(10,3)}`)
+console.log("El resultado 1 > " + conv(x,y))
