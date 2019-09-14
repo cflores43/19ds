727e2e966a050830740b7739c5da0ea3ecf6fb5d
cflores43 <51140829+cflores43@users.noreply.github.com>
2019-09-14 14:17:39 -0500

    Update 15.-fibonacci.js

--- 15.-fibonacci.js
+++ 15.-fibonacci.js
@@ -1,18 +1,20 @@
-let n =prompt(`por favor, introduce un numero`)
+let n =prompt("Cuantos numeros de Fibonacci quieres ver?")
 let xN = parseInt(n,10)
 
 let a = 1
 let b = 1
 var c 
-console.log(a)
-console.log(b)
-c   = b + a;
-console.log(c)
-for(var i = 3; i <= n-1;   i++)
- {
- a = b;
- b = c;
- c = b + a;
- console.log(c)
-
+function fib(n){
+    console.log(a)
+    console.log(b)
+    c   = b + a;
+    console.log(c)
+    for(var i = 3; i <= n-1;   i++){
+         a = b;
+         b = c;
+         c = b + a;
+         console.log(c)
+     }
 }
+
+fib(n)
